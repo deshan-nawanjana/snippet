17d07123f2ef3f89104c4f0f21703511f9390564
Deshan Nawanjana <deshan.uok@gmail.com>
2026-02-22 11:25:00 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,125 @@
+import { Snippet } from "./modules/Snippet.js"
+
+// helper to create elements
+const createElement = (className, content, parent) => {
+  // create element by type
+  const element = document.createElement("div")
+  // set element class name
+  element.className = className
+  // set element inner content
+  element.innerHTML = content
+  // append into parent if given
+  if (parent) { parent.appendChild(element) }
+  // return created element
+  return element
+}
+
+// get snippet view elements
+const tabsElement = document.querySelector(".tabs")
+const codeElement = document.querySelector(".code")
+const linesElement = document.querySelector(".lines")
+
+// method to initiate
+const init = async inputs => {
+  // get inputs as an array
+  inputs = Array.isArray(inputs) ? inputs : [inputs]
+  // create snippet
+  const snippet = new Snippet(inputs)
+  // load contents
+  const contents = await snippet.load()
+  // clear all tabs
+  tabsElement.innerHTML = ""
+  // method to select tab
+  const onSelect = (data, index) => {
+    // clear content elements
+    codeElement.innerHTML = ""
+    linesElement.innerHTML = ""
+    // set as current content
+    codeElement.innerHTML = data.content
+    // get previously selected tab
+    const previous = document.querySelector("[data-selected]")
+    // remove selected tab
+    if (previous) { previous.removeAttribute("data-selected") }
+    // get tab elements
+    const tabs = document.querySelectorAll(".tab-item")
+    // set current tab as selected
+    tabs[index].setAttribute("data-selected", true)
+    // get lines count
+    const lines = data.content.split("\n").length
+    // get start line number
+    const start = inputs[index].from ?? 1
+    // for each line
+    for (let n = 0; n < lines; n++) {
+      // create and append line element
+      createElement("line-item", start + n, linesElement)
+    }
+  }
+  // for each content tab
+  for (let i = 0; i < contents.length; i++) {
+    // get current content
+    const content = contents[i]
+    // get tab title
+    const title = content.title ?? content.url?.split("/").pop()
+    // create tab item
+    const item = createElement("tab-item", title, tabsElement)
+    // set tab title if url given
+    if (content.url) { item.setAttribute("title", content.url) }
+    // add tab item event
+    item.addEventListener("click", () => onSelect(content, i))
+  }
+  // select first item initially
+  onSelect(contents[0], 0)
+  // check if no title for only input
+  if (inputs.length === 1 && !inputs[0].title) {
+    // hide tabs element
+    tabsElement.remove()
+    // flag as no tabs element
+    document.body.setAttribute("data-tabs", false)
+  }
+  // set as ready
+  document.body.setAttribute("data-ready", true)
+}
+
+// get url search params
+const params = new URLSearchParams(location.search)
+
+// check for source param
+if (params.has("source")) {
+  // initiate with search params 
+  init({
+    source: params.get("source"),
+    language: params.get("language"),
+    from: parseInt(params.get("from")) || null,
+    to: parseInt(params.get("to")) || null,
+    title: params.get("title")
+  })
+}
+
+// check for source param
+if (params.has("source_1")) {
+  // inputs array
+  const inputs = []
+  // while source given
+  while (params.has("source_" + (inputs.length + 1))) {
+    // get current input index
+    const index = inputs.length + 1
+    // push options to inputs
+    inputs.push({
+      source: params.get("source_" + index),
+      language: params.get("language_" + index),
+      from: parseInt(params.get("from_" + index)) || null,
+      to: parseInt(params.get("to_" + index)) || null,
+      title: params.get("title_" + index)
+    })
+  }
+  // initiate multiple inputs
+  init(inputs)
+}
+
+// listen to post messages
+window.addEventListener("message", event => {
+  // return if not snippet data
+  if (!event.data || !event.data.isSnippet) { return }
+  // initiate with post message data
+  init(event.data.data)
+})
---
+++ modules/Snippet.js
@@ -0,0 +1,133 @@
+/** @typedef {{ source: string, language?: string, from?: number, to?: number, title?: string }} SnippetInput */
+/** @typedef {{ title: string | null, content: string, url?: string }} SnippetContent */
+
+/** @type {Object.<string, string>} Cache directory */
+const cache = {}
+
+/**
+ * Creates raw source url if available
+ * @param {string} source Source URL
+ */
+const toSourceURL = source => {
+  // parse source as url
+  const url = new URL(source)
+  // switch by origin
+  if (url.hostname === "github.com") {
+    // split source into parts
+    const parts = source.split("/")
+    // get url segments
+    const repo = parts.slice(3, 5).join("/")
+    const head = parts[6]
+    const file = parts.slice(7).join("/")
+    // return github raw url
+    return `https://raw.githubusercontent.com/${repo}/refs/heads/${head}/${file}`
+  } else if (url.hostname === "gitlab.com" && url.pathname.includes("/-/blob/")) {
+    // return gitlab raw url
+    return source.replace("/-/blob/", "/-/raw/")
+  }
+  // return same source
+  return source
+}
+
+/**
+ * Converts text content into snippet content
+ * @param {string} text Text content
+ * @param {SnippetInput} input Source input
+ */
+const toSnippetContent = (text, input, url) => {
+  // split content into lines
+  const lines = text.split("\n")
+  // remove last line if empty
+  if (!lines[lines.length - 1].trim()) { lines.pop() }
+  // get segment range
+  const from = input.from ?? 1
+  const to = input.to ?? lines.length
+  // get text segment content from options
+  const segment = lines.slice(from - 1, to).join("\n")
+  // get language from input options
+  const lang = input.language ?? url?.split(".").pop() ?? "plain"
+  // get highlighted content if prism library available
+  const content = typeof Prism === "object"
+    ? Prism.highlight(segment, Prism.languages[lang], lang)
+    : segment
+  // get title by input options
+  const title = "title" in input ? input.title : url?.split("/").pop() ?? null
+  // return content output
+  return { title, content, url }
+}
+
+/** Snippet Module */
+export class Snippet {
+  /**
+   * Snippet Module
+   * @param {SnippetInput | SnippetInput[]} inputs Source inputs
+   */
+  constructor(inputs) {
+    /** @type {SnippetInput | SnippetInput[]} Source inputs */
+    this.inputs = inputs ?? []
+  }
+  /**
+   * Loads snippet content
+   * @returns {Promise<SnippetContent | SnippetContent[]>}
+   */
+  async load() {
+    // get inputs as an array
+    const inputs = Array.isArray(this.inputs) ? this.inputs : [this.inputs]
+    // contents array
+    const contents = []
+    // for each source input
+    for (let i = 0; i < inputs.length; i++) {
+      // current input
+      const input = inputs[i]
+      // check if input source is an url
+      if (input.source.startsWith("https://") && !input.source.includes("\n")) {
+        // get source url
+        const url = toSourceURL(input.source)
+        // check for cache directory
+        if (url in cache) {
+          // create snippet from cache content
+          contents.push(toSnippetContent(cache[url], input, url))
+        } else {
+          const content = await fetch(url).then(resp => resp.text())
+          // store in cache
+          cache[url] = content
+          // create snippet content
+          contents.push(toSnippetContent(content, input, url))
+        }
+      } else {
+        // create snippet from text content
+        contents.push(toSnippetContent(input.source, input))
+      }
+    }
+    // return output
+    return Array.isArray(this.inputs) ? contents : contents[0]
+  }
+}
+
+// get embed host url
+const host = import.meta.url.replace("modules/Snippet.js", "")
+
+/**
+ * Creates a Snippet embed frame
+ * @param {SnippetInput | SnippetInput[]} inputs Source inputs
+ */
+export const createSnippet = inputs => {
+  // create frame element
+  const element = document.createElement("iframe")
+  // frame load listener
+  element.addEventListener("load", () => {
+    // post message source inputs
+    element.contentWindow.postMessage({
+      isSnippet: true,
+      data: inputs
+    })
+  })
+  // set embed host url
+  element.src = host
+  // return frame element
+  return element
+}
+
+// global accessibility
+window.Snippet = Snippet
+window.Snippet.createSnippet = createSnippet
